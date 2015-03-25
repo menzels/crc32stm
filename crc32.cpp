de9ca440182d112e5031b96b6657dc05caf88737
Stefan Menzel <menzel.stefan.d@googlemail.com>
2015-03-25 10:55:35 +0100

    initial commit
    Signed-off-by: Stefan Menzel <menzel.stefan.d@googlemail.com>

---
+++ crc32.cpp
@@ -0,0 +1,100 @@
+#include "crc32.h"
+
+Crc32::Crc32()
+{
+  cfg_.init = 0;
+  cfg_.polynomial = 0x04C11DB7;
+  generateLookup();
+}
+
+Crc32::Crc32(uint32_t init, uint32_t polynomial)
+{
+  crc_ = init;
+  cfg_.init = init;
+  cfg_.polynomial = polynomial;
+  generateLookup();
+}
+
+void Crc32::addData(const void* data, size_t length)
+{
+  size_t llwords = length      / 16;
+  size_t lwords  = length % 16 /  8;
+  size_t words   = length %  8 /  4;
+  size_t bytes   = length %  4;
+
+  const uint32_t* d = (const uint32_t*) data;
+
+  while(llwords--)
+  {
+    uint32_t one   = *d++ ^ crc_;
+    uint32_t two   = *d++;
+    uint32_t three = *d++;
+    uint32_t four  = *d++;
+    crc_  = lookup_[ 0][(four       ) & 0xFF] ^
+            lookup_[ 1][(four  >>  8) & 0xFF] ^
+            lookup_[ 2][(four  >> 16) & 0xFF] ^
+            lookup_[ 3][(four  >> 24) & 0xFF] ^
+            lookup_[ 4][(three      ) & 0xFF] ^
+            lookup_[ 5][(three >>  8) & 0xFF] ^
+            lookup_[ 6][(three >> 16) & 0xFF] ^
+            lookup_[ 7][(three >> 24) & 0xFF] ^
+            lookup_[ 8][(two        ) & 0xFF] ^
+            lookup_[ 9][(two   >>  8) & 0xFF] ^
+            lookup_[10][(two   >> 16) & 0xFF] ^
+            lookup_[11][(two   >> 24) & 0xFF] ^
+            lookup_[12][(one        ) & 0xFF] ^
+            lookup_[13][(one   >>  8) & 0xFF] ^
+            lookup_[14][(one   >> 16) & 0xFF] ^
+            lookup_[15][(one   >> 24) & 0xFF];
+  }
+
+  while(lwords--)
+  {
+    uint32_t one = *d++ ^ crc_;
+    uint32_t two = *d++;
+    crc_ =  lookup_[7][ one>>24        ] ^
+            lookup_[6][(one>>16) & 0xFF] ^
+            lookup_[5][(one>> 8) & 0xFF] ^
+            lookup_[4][ one      & 0xFF] ^
+            lookup_[3][ two>>24        ] ^
+            lookup_[2][(two>>16) & 0xFF] ^
+            lookup_[1][(two>> 8) & 0xFF] ^
+            lookup_[0][ two      & 0xFF];
+  }
+
+  while(words--)
+  {
+    uint32_t one = *d++ ^ crc_;
+
+    crc_ = lookup_[3][(one>>24) & 0xFF] ^
+           lookup_[2][(one>>16) & 0xFF] ^
+           lookup_[1][(one>> 8) & 0xFF] ^
+           lookup_[0][(one)     & 0xFF];
+  }
+
+  uint8_t* c = (uint8_t*) d;
+  while(bytes--)
+  {
+    crc_ = (crc_ << 8) ^ lookup_[0][(crc_>>24) ^ *c++];
+  }
+}
+
+void Crc32::generateLookup()
+{
+  for(size_t i = 0; i <= 0xFF; i++)
+  {
+    uint32_t r = i<<24;
+    for (size_t j=0; j<8; j++)
+      r = (r << 1) ^ ((r>>31 & 1) * cfg_.polynomial);
+    lookup_[0][i] = r;
+
+  }
+  for(size_t i = 0; i <= 0xFF; i++)
+  {
+    for (size_t slice = 1; slice < 16; slice++)
+    {
+      lookup_[slice][i] = (lookup_[slice-1][i] << 8) ^
+          lookup_[0][(lookup_[slice-1][i] >> 24) & 0xFF];
+    }
+  }
+}
---
+++ crc32.pri
@@ -0,0 +1,7 @@
+INCLUDEPATH += $$PWD
+
+CONFIG += c++11
+
+SOURCES += $$PWD/crc32.cpp
+
+HEADERS += $$PWD/crc32.h
